aa5d233c9eb78f20a8f21c1da64b1252592d90ad
l201010034-art <l201010034@itlerma.edu.mx>
2025-11-07 18:43:48 -0600

    Actualizacion Funcional v3.0.1

---
+++ www/capacitor.js
@@ -0,0 +1,15 @@
+/*! Capacitor: https://capacitorjs.com/ - MIT License */
+/* GENERATED FILE, DO NOT EDIT */
+
+var capacitorExports = (function (win) {
+    var capacitor = win.Capacitor || {};
+    var plugins = capacitor.Plugins || {};
+
+    // Forzar a que los plugins nativos se carguen si ya están disponibles
+    if (win.androidBridge) {
+        // Android específico
+    }
+
+    capacitor.Plugins = plugins;
+    return capacitor;
+})(window);
